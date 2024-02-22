20fb2518f7996582e1e54259a160ce948e15a7a9
oliverloops <oliveralr@protonmail.com>
2024-02-22 02:20:00 -0600

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+    },
+  },
+  plugins: [],
+};
