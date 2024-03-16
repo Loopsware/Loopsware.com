8ca51927538a310ff0e055eddb6a720b092bddb6
oliverloops <oliveralr@protonmail.com>
2024-03-15 23:32:48 -0600

    3D render update

--- src/computer_render.ts
+++ src/computer_render.ts
@@ -2,22 +2,25 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-// Create a scene
 const scene = new THREE.Scene();
 // Existing point light
-const light = new THREE.PointLight(0xffffff, 90, 100);
+const light = new THREE.PointLight(0xffffff, 50, 100);
 light.position.set(0, 0, 5);
 scene.add(light);
 
-// New point light for back illumination
-const backLight = new THREE.PointLight(0xffffff, 90, 100);
-backLight.position.set(0, 0, -5); // Position it behind the model
-scene.add(backLight);
+// Add a directional light from the top
+const topLight = new THREE.DirectionalLight(0xffffff, 2);
+topLight.position.set(0, 1, 0);
+scene.add(topLight);
 
-// Create a renderer
-// Create a renderer with alpha
+// Add a point light at the front of the scene with a yellow color
+const frontLight = new THREE.DirectionalLight(0x9400d3, 1);
+frontLight.position.set(0, 0, 1);
+scene.add(frontLight);
+
+// Create a renderer with anti-aliasing
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setClearColor(0x000000, 0); // the second parameter is the opacity, set it to 0 for full transparency
+renderer.setClearColor(0x000000, 0);
 renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
 document.body.appendChild(renderer.domElement);
 
@@ -31,9 +34,14 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 camera.position.z = 5;
+camera.position.x = 0;
+camera.position.y = 1.5;
+
+// Make the camera look at the center of the scene
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 let model: any;
-// Load a .glb file
+
 const loader = new GLTFLoader();
 loader.load(
   "/public/old_computer.glb",
@@ -47,28 +55,37 @@ loader.load(
   }
 );
 
+// Change the color of the left light to a fluorescent orange color
+const leftLight = new THREE.DirectionalLight(0xffa500, 3);
+leftLight.position.set(-1, 0, 0);
+scene.add(leftLight);
+
+// Change the color of the right light to a fluorescent purple color
+const rightLight = new THREE.DirectionalLight(0x9400d3, 2);
+rightLight.position.set(1, 0, 0);
+scene.add(rightLight);
+
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableZoom = false;
 controls.minPolarAngle = Math.PI / 4; // Limit the camera to 45 degrees above the horizon
 controls.maxPolarAngle = Math.PI / 2; // Limit the camera to 90 degrees above the horizon
 controls.minAzimuthAngle = -Math.PI / 4; // Limit the camera to 45 degrees to the left
-controls.maxAzimuthAngle = Math.PI / 4; // Limit the camera to 45 degrees to the right
+controls.maxAzimuthAngle = Math.PI / 5; // Limit the camera to 45 degrees to the right
 
 // Set the target to the model's position
 if (model) {
   controls.target.set(model.position.x, model.position.y, model.position.z);
 }
 
-let time = 0; // Initialize a time variable
+let time = 0;
 
-// Animation loop
 function animate() {
   requestAnimationFrame(animate);
 
   // Check if the model is loaded
   if (model) {
-    // Rotate the model back and forth between -0.4 and 0.4
-    model.rotation.x = Math.sin(time) * 0.25;
+    // Rotate the model back and forth between -0.25 and 0.25
+    model.rotation.y = Math.sin(time) * 0.2;
     time += 0.005; // Increase time
   }
 
@@ -78,8 +95,5 @@ function animate() {
 
 animate();
 
-// Select the container element
 const container = document.querySelector("#scene");
-
-// Append the renderer to the container instead of the body
 container?.appendChild(renderer.domElement);
