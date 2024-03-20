c714dd44daae71ab20f99397ae68be65e4164b30
oliverloops <oliveralr@protonmail.com>
2024-03-19 23:08:16 -0600

    Adding i18n translations

---
+++ src/translations.ts
@@ -0,0 +1,19 @@
+// translations.js
+export const translations = {
+  en: {
+    navbar: {
+      services: "Services",
+      pricing: "Pricing",
+      about: "About",
+      button: "Book a call",
+    },
+  },
+  es: {
+    navbar: {
+      services: "Servicios",
+      pricing: "Precios",
+      about: "Acerca de",
+      button: "Hablemos",
+    },
+  },
+};
