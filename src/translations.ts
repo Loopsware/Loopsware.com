c1b76ebb966feaae22793af43522d5b7e3fbe813
oliverloops <oliveralr@protonmail.com>
2024-03-19 23:25:24 -0600

    Adding i18n translations

--- src/translations.ts
+++ src/translations.ts
@@ -7,6 +7,34 @@ export const translations = {
       about: "About",
       button: "Book a call",
     },
+    services: {
+      serviceOne: {
+        titleLineOne: "Pitch your idea.",
+        titleLineTwo: "Let's make it real.",
+        subtitleLineOne:
+          "If your starting out, we can help you with the ideation,",
+        subtitleLineTwo:
+          "planning, design, development and publishing of your app.",
+        items: {
+          planning: "Planning",
+          design: "Design",
+          development: "Development",
+          publication: "Publishing",
+        },
+      },
+      servicesTwo: {
+        titleLineOne: "Need an extra hand?",
+        titleLineTwo: "Let us help with it.",
+        subtitleLineOne: "Do you already have a project?, let's work together",
+        subtitleLineTwo: "we can provide you support or mantainance.",
+        items: {
+          planning: "Bug Fixing",
+          design: "Live Support",
+          development: "Native Modules",
+        },
+      },
+      button: "Check pricing",
+    },
   },
   es: {
     navbar: {
@@ -14,6 +42,35 @@ export const translations = {
       pricing: "Precios",
       about: "Acerca de",
       button: "Hablemos",
+    },
+    services: {
+      serviceOne: {
+        titleLineOne: "Cuentanos tu idea.",
+        titleLineTwo: "La hacemos realidad.",
+        subtitleLineOne:
+          "Si estás empezando, podemos ayudarte con la ideación,",
+        subtitleLineTwo:
+          "planificación, diseño, desarrollo y publicación de su aplicación.",
+        items: {
+          planning: "Planificación",
+          design: "Diseño",
+          development: "Desarrollo",
+          publication: "Publicación",
+        },
+      },
+      servicesTwo: {
+        titleLineOne: "¿Necesitas apoyo?",
+        titleLineTwo: "Nosotros te ayudamos.",
+        subtitleLineOne: "¿Tienes ya algún proyecto?. trabajemos juntos",
+        subtitleLineTwo:
+          "podemos proveer soporte técnico, mantenimiento y actualizaciones.",
+        items: {
+          planning: "Solución a bugs",
+          design: "Soporte en vivo",
+          development: "Modulos nativos",
+        },
+      },
+      button: "Ver precios",
     },
   },
 };
