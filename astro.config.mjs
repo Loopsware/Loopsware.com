e5cbb3f80885137eeba93d652fdeaf1bc0042ae2
oliverloops <oliveralr@protonmail.com>
2024-03-25 03:08:50 -0600

    React reconfigured

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), react()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
     routing: {
-      prefixDefaultLocale: false,
-    },
-  },
+      prefixDefaultLocale: false
+    }
+  }
 });
