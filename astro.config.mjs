87ef6f7919ee4c8d5215f8da5fc23bc205807741
oliverloops <oliveralr@protonmail.com>
2024-06-23 16:13:28 -0600

    Vercel analytics and speed insigths added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   integrations: [tailwind(), mdx(), react({ include: ["**/react/*"] })],
   i18n: {
     defaultLocale: "en",
