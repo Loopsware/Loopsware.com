381cc37ba965167dec4174f6241aea479b0baf65
oliverloops <oliveralr@protonmail.com>
2025-03-12 02:00:10 -0600

    Adding animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,8 +7,17 @@ export default {
         "fade-in-up": "fadeInUp 0.6s ease-out forwards",
         "fade-in-left": "fadeInLeft 0.6s ease-out forwards",
         "fade-in-right": "fadeInRight 0.6s ease-out forwards",
+        levitate: "levitate 5s ease-in-out infinite",
       },
       keyframes: {
+        levitate: {
+          "0%, 100%": {
+            transform: "translateY(0)",
+          },
+          "50%": {
+            transform: "translateY(-16px)",
+          },
+        },
         fadeInUp: {
           "0%": {
             opacity: "0",
